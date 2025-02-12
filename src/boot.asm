f54aa590382c35858879c209bbd0dc71e523172c
Joe <167563398+Joercat@users.noreply.github.com>
2025-02-12 12:44:54 -0500

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,14 +1,39 @@
 section .text
 global _start
-extern main
+extern kernel_main
 
 _start:
-    call init_platform
-    call main
+    ; Set up stack
+    mov rsp, stack_top
     
-init_platform:
-    push rbp
-    mov rbp, rsp
-    ; 64-bit platform initialization
-    pop rbp
+    ; Clear interrupts during init
+    cli
+    
+    ; Initialize essential CPU state
+    call setup_long_mode
+    call setup_paging
+    
+    ; Jump to kernel
+    call kernel_main
+    
+    ; Should never return, but halt if it does
+    hlt
+
+setup_long_mode:
+    ; Enable PAE
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+    
+    ; Set long mode bit
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
     ret
+
+section .bss
+align 4096
+stack_bottom:
+    resb 16384  ; 16 KB stack
+stack_top:
