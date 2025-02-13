8594fdfcae146a27372e57f46f71809276345bea
Joe <167563398+Joercat@users.noreply.github.com>
2025-02-13 09:39:23 -0500

    Update stdint.h

--- src/stdint.h
+++ src/stdint.h
@@ -12,6 +12,18 @@ typedef unsigned long long uint64_t;
 
 typedef uint64_t size_t;
 typedef int64_t ssize_t;
+typedef uint64_t uintptr_t;
+typedef int64_t intptr_t;
+
+#define UINT8_MAX   0xff
+#define UINT16_MAX  0xffff
+#define UINT32_MAX  0xffffffff
+#define UINT64_MAX  0xffffffffffffffff
+
+#define INT8_MAX    127
+#define INT16_MAX   32767
+#define INT32_MAX   2147483647
+#define INT64_MAX   9223372036854775807
 
 #define NULL ((void*)0)
 
